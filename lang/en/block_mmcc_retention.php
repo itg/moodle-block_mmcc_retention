94f1c74d4505996fd99c6a66507b2a7b9bd3e296
Brandon Kish <brandonkish@me.com>
2018-07-11 16:00:56 -0400

    Replace "MMCC" with "Mid" per new college name and brand guidelines (#4)
    * Replace "MMCC" with "Mid" per new college name and brand guidelines
    
    Also bump version number

--- lang/en/block_mmcc_retention.php
+++ lang/en/block_mmcc_retention.php
@@ -1,12 +1,12 @@
 <?php
-$string["pluginname"] = "MMCC RMS block";
-$string["mmcc_retention"] = "MMCC RMS";
-$string[""] = "MMCC RMS block";
-$string["mmcc_retention:addinstance"] = "Add a new MMCC RMS block";
-$string["mmcc_retention:myaddinstance"] = "Add a new MMCC RMS block to the My Moodle page";
+$string["pluginname"] = "Mid RMS block";
+$string["mmcc_retention"] = "Mid RMS";
+$string[""] = "Mid RMS block";
+$string["mmcc_retention:addinstance"] = "Add a new Mid RMS block";
+$string["mmcc_retention:myaddinstance"] = "Add a new Mid RMS block to the My Moodle page";
 
 
 $string["headerconfig"] = "API Token";
 $string["descconfig"] = "";
 $string["configapitokentitle"] = "API Token";
-$string["configapitokendesc"] = "The token used to authenticate requests to MMCC's Referral Management System.";
+$string["configapitokendesc"] = "The token used to authenticate requests to Mid's Referral Management System.";
